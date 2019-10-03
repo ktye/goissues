220ad98fea3cb667abe947e9a4871cfa7856494d
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2019-10-03 13:50:46 +0200

    gioui questionmark

--- gio/39/main.go
+++ gio/39/main.go
@@ -13,7 +13,7 @@ import (
 	"gioui.org/op/paint"
 )
 
-var Image *image.RGBA // This image is modified then uploaded to the window buffer at each event.
+var Image *image.RGBA // This image is modified then uploaded to the window buffer at each click.
 
 func main() {
 	go func() {
@@ -37,12 +37,12 @@ func main() {
 }
 
 func Draw(ops *op.Ops, size image.Point) {
-	fmt.Println("Draw")
 	if Image.Bounds().Max != size {
 		fmt.Println("Resize ", size)
 		Image = image.NewRGBA(image.Rectangle{Max: size}) // reallocate backing image with new size
 	}
-	drawNextFrame() // change the color of the visible rectangle
+	c := drawNextFrame() // modifies Image: it changes the color of the visible rectangle on each call.
+	fmt.Println("Draw, color:", c)
 	paint.ImageOp{Image, Image.Bounds()}.Add(ops)
 	paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{float32(size.X), float32(size.Y)}}}.Add(ops)
 }
@@ -51,14 +51,13 @@ var frameIndex int
 var red, green, blue = color.RGBA{255, 0, 0, 255}, color.RGBA{0, 255, 0, 255}, color.RGBA{0, 0, 255, 255}
 
 // DrawNextFrame paints the upper left quadrant of the backing image red, green or blue.
-func drawNextFrame() {
+func drawNextFrame() color.Color {
 	colors := []color.RGBA{red, green, blue}
 	c := colors[frameIndex%3]
-	fmt.Println("set frame color", c)
 	r := Image.Bounds()
 	r.Max.X /= 2
 	r.Max.Y /= 2
 	draw.Draw(Image, r, &image.Uniform{c}, image.ZP, draw.Src)
 	frameIndex++
-	//window.Invalidate() // Is this the way to trigger a new UpdateEvent?
+	return c
 }
